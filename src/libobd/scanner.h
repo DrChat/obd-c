b055c324962386f734dd580cc361ad136b422dad
Justin Moore <arkolbed@gmail.com>
2019-08-30 17:52:43 -0500

    WIP: Scanner class

---
+++ src/libobd/scanner.h
@@ -0,0 +1,18 @@
+/**
+ ******************************************************************************
+ * OBD-C : CAN BUS Research Project                                           *
+ ******************************************************************************
+ * Copyright 2019 Justin Moore. All rights reserved.                          *
+ ******************************************************************************
+ */
+#pragma once
+
+/**
+ * \brief Abstract scanner class. A transport communicates with this to retrieve
+ * information from the CAN bus.
+ */
+class Scanner {
+    public:
+
+    private:
+};
