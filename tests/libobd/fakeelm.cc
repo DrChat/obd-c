4c46ff14aa52a7f3d63acdc5b5ec878245dfc68e
Justin Moore <arkolbed@gmail.com>
2019-09-03 18:56:54 -0500

    [Test] Real first test for fakeelm

--- tests/libobd/fakeelm.cc
+++ tests/libobd/fakeelm.cc
@@ -1,16 +1,21 @@
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 #include "libobd/fakeelm.h"
 #include "libobd/faketransport.h"
 
+using testing::_;
+using testing::MockFunction;
+using testing::Return;
+
 TEST(FakeELM, BasicTest) {
+  MockFunction<void(const uint8_t*, size_t)> mockReceive;
   FakeTransport t;
   FakeELM327 e;
 
-  t.SetReceiveHandler([](const uint8_t* buf, size_t len) {
-	  printf("%.*s", (int)len, buf);
-  });
-
+  t.SetReceiveHandler(mockReceive.AsStdFunction());
   t.Connect(&e);
+
+  EXPECT_CALL(mockReceive, Call(_, _));
   t.Send("ATI\r");
 }
